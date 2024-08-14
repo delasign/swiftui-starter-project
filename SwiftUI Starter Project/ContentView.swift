11715a973dcf5b230cc7dd39ab3a4c4809adf311
Oscar de la Hera Gomez <oscar@delasign.com>
2024-08-14 13:57:01 -0400

    Initial Commit

---
+++ SwiftUI Starter Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 8/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Starter Project/SwiftUI_Starter_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Starter_ProjectApp.swift
+//  SwiftUI Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Starter_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
