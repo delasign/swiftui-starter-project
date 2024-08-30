a74c92b22221f5115c8261ec46ceb9c7e9f49c3b
Oscar de la Hera Gomez <oscar@delasign.com>
2024-08-30 12:03:42 -0400

    JSON Utils

---
+++ SwiftUI Starter Project/Utils/JSON/JSONToData.swift
@@ -0,0 +1,20 @@
+//
+//  JSONToData.swift
+//  SwiftUI Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 8/30/24.
+//
+
+import Foundation
+
+func convertJSONToData<T: Encodable>(item: T) -> Data? {
+    do {
+        let encodedJSON = try JSONEncoder().encode(item)
+        debugPrint("convertJSONToData \(DebuggingIdentifiers.actionOrEventSucceded) data : \(encodedJSON)")
+        return encodedJSON
+    } catch {
+        debugPrint("convertJSONToData \(DebuggingIdentifiers.actionOrEventFailed) error : \(error)")
+        return nil
+    }
+}
+
