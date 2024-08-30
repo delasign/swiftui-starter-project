44395530ca5de3af729b2adef3eaffc93e53ed73
Oscar de la Hera Gomez <oscar@delasign.com>
2024-08-30 12:09:05 -0400

    + Base Utils

---
+++ SwiftUI Starter Project/Utils/UIApplication/openSettings.swift
@@ -0,0 +1,14 @@
+//
+//  openSettings.swift
+//  SwiftUI Starter Project
+//
+//  Created by Oscar de la Hera Gomez on 8/30/24.
+//
+
+import Foundation
+import UIKit
+
+func openSettings() {
+    openURL(url: UIApplication.openSettingsURLString)
+}
+
