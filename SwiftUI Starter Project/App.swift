52063ccae3d442599fcef284edaae1002bd84816
Oscar de la Hera Gomez <oscar@delasign.com>
2024-10-18 14:00:52 -0400

    Complete

--- SwiftUI Starter Project/App.swift
+++ SwiftUI Starter Project/App.swift
@@ -10,7 +10,7 @@ import SwiftUI
 // Please note that "Application" cannot be named App.
 @main
 struct Application: App {
-    
+    @Environment(\.scenePhase) var scenePhase
     @State private var languageCoordinator = LanguageCoordinator()
     let styleguide = Styleguide()
     
@@ -18,6 +18,20 @@ struct Application: App {
         WindowGroup {
             SampleScreen()
                 .environment(languageCoordinator)
+        }.onChange(of: scenePhase) { oldPhase, newPhase in
+            switch newPhase {
+                case .active:
+                    debugPrint("\(DebuggingIdentifiers.notificationRecieved) App is Active")
+                break
+                case .background:
+                    debugPrint("\(DebuggingIdentifiers.notificationRecieved) App went to the background")
+                break
+                case .inactive:
+                    debugPrint("\(DebuggingIdentifiers.notificationRecieved) App is Inactive")
+                break
+            @unknown default:
+                fatalError("Unknown scene phase: \(newPhase)")
+            }
         }
     }
 }
